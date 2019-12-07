daf0a3c51f9709ba154f00b7ba2b3273b7642eb3
RITWICK KUMAR <ritwickkumar210gmail.com>
2019-12-08 04:42:24 +0530

    dark mode

---
+++ js5.js
@@ -0,0 +1,12 @@
+<!--
+	var activeTab = 2;
+	function openTab(tabId) {
+		// reset old tab and content
+		document.getElementById("tabLink"+activeTab).className = "tabLink";
+		document.getElementById("tabContent"+activeTab).className = "tabContent";
+		// set new tab and content
+		document.getElementById("tabLink"+tabId).className = "tabLinkActive";
+		document.getElementById("tabContent"+tabId).className = "tabContentActive";
+		activeTab = tabId;
+	}
+-->
